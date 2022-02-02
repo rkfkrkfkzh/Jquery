02789261c0996f544f4cdc0c6efc00e849541b8d
Hyojin Lim <1004dlagywls@naver.com>
2022-02-03 01:12:24 +0900

    ok

--- 1.Jquery/src/main/java/step1/WelcomeServlet.java
+++ 1.Jquery/src/main/java/step1/WelcomeServlet.java
@@ -13,8 +13,9 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class WelcomeServlet
  */
 @WebServlet("/WelcomeServlet")
-public class WelcomeServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+public class WelcomeServlet extends HttpServlet { // 실제 예를 들기 위해 WelcomeServlet이라는 클래스를 
+	// HttpServlet 인터페이스를 받아서 구현을 합니다.
+	private static final long serialVersionUID = 1L; // serialVersionUID 가 선언되어있지 않을 경우 default로 값을 만들어준다
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -37,10 +38,11 @@ public class WelcomeServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.setContentType("text/html;charset=utf-8");
-		request.setCharacterEncoding("utf-8");
+		response.setContentType("text/html;charset=utf-8"); //문자열 형태의 type에 지정된 MIME Type으로 Content Type을 지정한다.
+		request.setCharacterEncoding("utf-8"); // 현재 JSP로 전달되는 내용을 지정한 캐리터셋을로 변환해 준다. 
+		// HTML 폼에서 한글을 입력할 때 정상적으로 처리하려면 반드시 필요하다.
 		// 응답시 문서타입과 한글처리
-		response.setContentType("text/html;charset=utf-8");
+		response.setContentType("text/html;charset=utf-8"); //문자열 형태의 type에 지정된 MIME Type으로 Content Type을 지정한다.
 		PrintWriter out = response.getWriter();
 		out.println("post 방식으로 요청했군요!!!");
 		//클라이언트의 요청 정보에서 아이디를 얻어온다.
